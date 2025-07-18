5cd736ff4761289f9ee16c7d6a18e5d18cd9f85c
Irvna <89928660+Irvna@users.noreply.github.com>
2025-07-18 15:52:45 -0300

    mensagens/

---
+++ cadastro_sucesso.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" href="img/1.png" type="imagex/x-icon">
+        <title>Show do Milhão</title>
+        <link rel="stylesheet" href="../css/style_inserir.css">
+    </head>
+    <body>
+        <div class="msg">
+            <h1>Cadastro de questão realizado com sucesso!</h1>
+            <br>
+            <br>
+            <div>
+                <a href='../area_jogo.php' class="BotaoMsg">Ir para o Jogo</a></br>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ campos_devem_preenchidos.php
@@ -0,0 +1,14 @@
+<head>
+    <link rel="icon" href="img/1.png" type="imagex/x-icon">
+    <title>Show do Milhão</title>
+    <link rel="stylesheet" href="./css/style_inserir.css"> </head>
+</head>
+
+<div class="msg">
+    <h1>Todos os campos devem ser preenchidos!</h1>
+    <br>
+    <br>
+    <div>
+        <a href="insere_questao.php" class="BotaoMsg">Voltar</a>
+    </div>
+</div>
