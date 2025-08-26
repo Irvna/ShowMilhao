87324ccd8d27d7a02cb2bd20670ad044f57334a7
Irvna <89928660+Irvna@users.noreply.github.com>
2025-08-26 08:54:21 -0300

    Add files via upload

--- novo_jogo.php
+++ novo_jogo.php
@@ -17,9 +17,11 @@ $_SESSION['categoria'] = $_POST['categoria-escolhida'];
 
 //conta quantas questões tem no banco. Ajustar para categorias.
 $SQL = "SELECT COUNT(*) AS total FROM questoes_show_do_milhao";
-if (strlen($_SESSION['categoria']) > 0) {
-    $cat = $_SESSION['categoria'];
-    $SQL = $SQL . " WHERE categoria ='$cat'";
+if ($_SESSION['categoria'] !== "aleatorio") {
+    if (strlen($_SESSION['categoria']) > 0) {
+        $cat = $_SESSION['categoria'];
+        $SQL = $SQL . " WHERE categoria ='$cat'";
+    }
 }
 
 $res = $conexao->query($SQL);
@@ -32,23 +34,23 @@ if ($res['total'] > 0) {
     //Sorteia uma questão aleatória da dificuldade desejada
     //o select retorna apenas um id e a dificuldade de maneira aleatória
     $categoria = $_SESSION['categoria'];
-    if ($_SESSION['categoria'] === "aleatorio") {
+    if ($categoria == "aleatorio") {
         $sql = "SELECT id, dificuldade FROM questoes_show_do_milhao 
                 WHERE dificuldade = '{$_SESSION['dificuldade']}' 
                 ORDER BY RAND() 
-                 LIMIT 1";
+                LIMIT 1";
     } else {
         $sql = "SELECT id, dificuldade FROM questoes_show_do_milhao 
-                    WHERE dificuldade = '{$_SESSION['dificuldade']}' and categoria = '$categoria'
-                    ORDER BY RAND() 
-                    LIMIT 1";
+                WHERE dificuldade = '{$_SESSION['dificuldade']}' and categoria = '$categoria'
+                ORDER BY RAND() 
+                LIMIT 1";
     }
     $res2 = $conexao->query($sql); //executa a query
     $linha = $res2->fetch_assoc(); //pega o resultado
     $questao_id = $linha['id']; //id da questão sorteada
     $_SESSION['questao_atual'] = $questao_id; //define a questão atual
     //armazena a questão atual no array de perguntas sorteadas
-    array_push($_SESSION['perguntas_sorteadas'], $_SESSION['questao_atual']); 
+    array_push($_SESSION['perguntas_sorteadas'], $_SESSION['questao_atual']);
 }
 //retorna para a area de jogo
 header("location:area_jogo.php");
